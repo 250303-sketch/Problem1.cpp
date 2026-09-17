1dd09b566536ebe75a6d876269bba4a5d4fb5f4d
Adhambek <user@users-MacBook-Air-2.local>
2026-09-17 11:56:36 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>//input and output stream
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+int main() {
+    std::cout << "Hello, World!!" << std::endl;
+    std::cout << "Another Hello World" << std::endl;
+    return 0;
+
+    }
+
